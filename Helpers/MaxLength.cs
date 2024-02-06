41c7340b3fa5fe797ebc7ba83b96719a99a78309
thuraphyo <thuraphyo@mingalarinfotech.com>
2024-02-07 01:00:57 +0630

    enhancement: added new max length fields

--- Helpers/MaxLength.cs
+++ Helpers/MaxLength.cs
@@ -5,5 +5,9 @@
         public const int Max_100 = 100;
         public const int Max_450 = 450;
         public const int Max_12 = 12;
+        public const int Max_20 = 20;
+        public const int Max_50 = 50;
+        public const int Max_1000 = 1000;
+        public const int Max_500 = 500;
     }
 }
